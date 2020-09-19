fcd0504c8a9692b559395a0b52e532d8c6c14c22
Stefan Stieber <s.stieber@me.com>
2020-09-19 21:29:56 +0200

    data aossications pt2

--- references.js
+++ references.js
@@ -11,25 +11,25 @@ var Post = require("./models/posts");
 
 var User = require("./models/users");
 
-// Post.create({
-//     title: "How to cook the best burger Pt. 3",
-//     content: "sdasdafadfsdfsdfkasjdkasf asaaiosd asidasoidjasoif oiufoaifaiosf"
-// }, function(err, post){
-//     User.findOne({email: "bob@gmail.com"}, function(err, foundUser){
-//         if(err){
-//             console.log(err);
-//         } else {
-//             foundUser.posts.push(post);
-//             foundUser.save(function(err, data){
-//                 if(err){
-//                     consol.log(err);
-//                 } else {
-//                     console.log(data);
-//                 }
-//             });
-//         }
-//     });
-// });
+Post.create({
+    title: "How to cook the best burger Pt. 4",
+    content: "hghghg jgjhgjg jgjgjg"
+}, function(err, post){
+    User.findOne({email: "bob@gmail.com"}, function(err, foundUser){
+        if(err){
+            console.log(err);
+        } else {
+            foundUser.posts.push(post);
+            foundUser.save(function(err, data){
+                if(err){
+                    consol.log(err);
+                } else {
+                    console.log(data);
+                }
+            });
+        }
+    });
+});
 
 // User.findOne({email: "bob@gmail.com"}).populate("posts").exec(function(err, user){
 //     if(err){
